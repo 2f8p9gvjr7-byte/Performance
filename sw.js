b1cc7b0575667b671e2218d9cbd61d7a91b6c6b6
2f8p9gvjr7-byte <2f8p9gvjr7@privaterelay.appleid.com>
2026-06-11 20:14:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'eval-bonus-v3';
+const CACHE = 'eval-bonus-v4';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
